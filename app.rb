38b3b1700186de1c04a75db02efa9985a47a27a3
stephane delatorre <s.dlt@hotmail.fr>
2017-09-12 19:27:51 +0200

    dynamic connection to new channel ok

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require_relative 'lib/twitch_bot'
 require_relative 'lib/game'
 require_relative 'lib/db'
 
+bot = TwitchBot.new(channels: Bot.pluck(:channel))
 
-bot = TwitchBot.new(channels: %w[monsieursapin])
-
-p bot.run
-p bot.running
+bot.update_channels
+bot.run
--- lib/twitch_bot.rb
+++ lib/twitch_bot.rb
@@ -17,18 +17,19 @@ class TwitchBot
     @channels = attr[:channels]
     @twitch_token = ENV['TWITCH_TOKEN']
     @boss = nil
-    @heroku_bot = nil
+    @heroku_bot = []
     @running = false
+    @connected_channels = []
   end
 
   def run
+    p @channel_updater
     @running = true
     logger.info("Innitializing bot #{inspect}...")
     initialize_bot
-    @heroku_bot = Bot.find_by(channel: @channel)
     until @socket.eof?
       line = @socket.gets
-      logger.info("> #{line}")
+      # logger.info("> #{line}")
 
       ping = line.match(/^PING :(.*)$/)
       match = line.match(/.*;display-name=(?<username>\w*);.*PRIVMSG #(?<channel>.+) :(?<message>.+)/)
@@ -41,27 +42,30 @@ class TwitchBot
       if ping
         send_to_twitch("PONG #{ping[1]}")
       elsif usernotice
+        p @channel_updater.alive?
         sub_match = line.match(/.*;display-name=(?<username>\w*).*msg-id=(?<type>\w*);msg-param-months=(?<month>\w*).*;msg-param-sub-plan=(?<plan>\w*).* USERNOTICE #(?<channel>\w*)( :(?<message>.*))?/)
         logger.info("LINE => #{line}")
         username = sub_match[:username]
         username = line.match(/.*;system-msg=(?<username>\w*).*/) if username.empty?
-        logger.info("SUB => username: #{username}, type: #{sub_match[:type]}, plan: #{sub_match[:plan]}, month: #{sub_match[:month]}, message: #{sub_match[:message] || 'nil'}")
+        logger.info("SUB => username: #{username}, type: #{sub_match[:type]}, channel: #{sub_match[:channel]}, plan: #{sub_match[:plan]}, month: #{sub_match[:month]}, message: #{sub_match[:message] || 'nil'}")
         # @boss.new_event(sub: { username: sub_match[:username], plan: sub_match[:plan] }) if live_state?
-        RestClient.patch(
-          "#{ENV['HEROKU_DOMAIN']}/bosses/#{@heroku_bot.boss.id}",
-          token: @heroku_bot.token,
-          bot_id: @heroku_bot.id,
-          event: {
-            boss: @heroku_bot.boss.id,
-            event_type: 'sub',
-            channel: sub_match[:channel],
-            username: username,
-            type: sub_match[:type],
-            plan: sub_match[:plan],
-            month: sub_match[:month],
-            message: sub_match[:message] || 'nil'
-          }
-        ) if live_state?
+        heroku_bot = find_bot(sub_match[:channel])
+        if live_state?(sub_match[:channel])
+          RestClient.patch(
+            "#{ENV['HEROKU_DOMAIN']}/bosses/#{heroku_bot.boss.id}",
+            token: heroku_bot.token,
+            bot_id: heroku_bot.id,
+            event: {
+              event_type: 'sub',
+              channel: sub_match[:channel],
+              username: username,
+              type: sub_match[:type],
+              plan: sub_match[:plan],
+              month: sub_match[:month],
+              message: sub_match[:message] || 'nil'
+            }
+          )
+        end
       elsif message =~ /!stop/
         stop!
       elsif message =~ /!start/
@@ -69,33 +73,39 @@ class TwitchBot
       elsif bits
         username = line.match(/@(?<username>\w*).tmi.twitch.tv/) if username.empty?
         logger.info("LINE => #{line}")
-        logger.info("BITS => username: #{bits[:username]}, total: #{bits[:amount]}")
+        logger.info("BITS => username: #{bits[:username]}, channel: #{bits[:channel]}, total: #{bits[:amount]}")
         # @boss.new_event(bits: { username: bits[:username], amount: bits[:amount] }) if live_state?
-        RestClient.patch(
-          "#{ENV['HEROKU_DOMAIN']}/bosses/#{@heroku_bot.boss.id}",
-          token: @heroku_bot.token,
-          bot_id: @heroku_bot.id,
-          event: {
-            event_type: 'bits',
-            channel: bits[:channel],
-            username: bits[:username],
-            amount: bits[:amount],
-            message: bits[:message] || 'nil'
-          }
-        ) if live_state?
+        heroku_bot = find_bot(bits[:channel])
+        if live_state?(sub_match[:channel])
+          RestClient.patch(
+            "#{ENV['HEROKU_DOMAIN']}/bosses/#{heroku_bot.boss.id}",
+            token: heroku_bot.token,
+            bot_id: heroku_bot.id,
+            event: {
+              event_type: 'bits',
+              channel: bits[:channel],
+              username: bits[:username],
+              amount: bits[:amount],
+              message: bits[:message] || 'nil'
+            }
+          )
+        end
       end
     end
     @running = false
   end
 
-  def live_state?
-    api_call = RestClient.get("https://api.twitch.tv/kraken/streams/#{@channel}?client_id=#{ENV['TWITCH_CLIENT_ID']}")
+  def live_state?(channel)
+    api_call = RestClient.get("https://api.twitch.tv/kraken/streams/#{channel}?client_id=#{ENV['TWITCH_CLIENT_ID']}")
     parsed_api_call = JSON.parse(api_call)
     stream = parsed_api_call['stream']
-    unless stream.nil?
-      stream_type = stream['stream_type']
-      stream_type == 'live'
-    end
+    return if stream.nil?
+    stream_type = stream['stream_type']
+    stream_type == 'live'
+  end
+
+  def find_bot(channel)
+    Bot.find_by(channel: channel)
   end
 
   def send_to_twitch(message)
@@ -117,6 +127,20 @@ class TwitchBot
     # @running = false
   end
 
+  def update_channels
+    @channel_updater = Thread.new do
+      loop do
+        sleep 30
+        Bot.pluck(:channel).each do |channel|
+          next if @connected_channels.include? channel
+          send_to_twitch("JOIN ##{channel}")
+          logger.info("connected to ##{channel}")
+          @connected_channels << channel
+        end
+      end
+    end
+  end
+
   def initialize_bot
     logger.info('Preparing to connect...')
     @socket = TCPSocket.new('irc.chat.twitch.tv', 6667)
@@ -126,6 +150,7 @@ class TwitchBot
     @channels.each do |channel|
       send_to_twitch("JOIN ##{channel}")
       logger.info("connected to ##{channel}")
+      @connected_channels << channel
     end
     send_to_twitch('CAP REQ :twitch.tv/membership')
     send_to_twitch('CAP REQ :twitch.tv/commands')
